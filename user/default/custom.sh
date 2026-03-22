a01d00d57e3b201329214b0171d721540019d188
fanboy <fanboy@openwrt.org>
2026-03-22 13:28:43 -1000

    luci-app-airoha-npu now regular patch; delete special handling

--- user/default/custom.sh
+++ user/default/custom.sh
@@ -2,12 +2,9 @@
 echo "=============================================="
 echo "Running custom commands"
 mv files/overview.js feeds/luci/applications/luci-app-attendedsysupgrade/htdocs/luci-static/resources/view/attendedsysupgrade/overview.js
-cp -r files/luci-app-airoha-npu feeds/luci/applications/
-rm -rf files/luci-app-airoha-npu
 cp -r files/luci-app-w1700k-fancontrol feeds/luci/applications/
 rm -rf files/luci-app-w1700k-fancontrol
 ./scripts/feeds update luci
-./scripts/feeds install luci-app-airoha-npu
 ./scripts/feeds install luci-app-w1700k-fancontrol
 rm -rf tmp/*
 echo "=============================================="
